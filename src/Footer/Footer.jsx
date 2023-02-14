b233b3fe0a0c8e887403492747a8a065be72c915
anthraXbogdan <bogdanenache07@gmail.com>
2023-02-14 14:03:10 +0200

    implement mock elements for footer component

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,29 @@
 import "./Footer.css";
 
 export default function Footer() {
-   return <footer></footer>;
+   return (
+      <footer>
+         <div className="footer-container">
+            <ul>
+               <li>
+                  <a href="#">Politica cookies</a>
+               </li>
+               <li>
+                  <a href="#">Politică de confidențialitate</a>
+               </li>
+               <li>
+                  <a href="#">Politica de retur</a>
+               </li>
+               <li>
+                  <a href="#">Livrarea produselor</a>
+               </li>
+               <li>
+                  <a href="#">Termeni și condiții</a>
+               </li>
+            </ul>
+
+            <p className="footer-copyright">&#169; petsidestory 2023</p>
+         </div>
+      </footer>
+   );
 }
